a436590f29f243d531f07291f336d4e58a6921b7
Sahib Aliyev <sahib.aliyev.2002@gmail.com>
2023-03-16 23:08:25 +0400

    Done

--- Program.cs
+++ Program.cs
@@ -1,10 +1,34 @@
-﻿namespace HttpClient
+﻿using System.Net.Http.Json;
+
+namespace HttpClient
 {
-    internal class Program
+    public class Program
     {
-        static void Main(string[] args)
+        static async Task Main()
         {
-            Console.WriteLine("Hello, World!");
+            var client = new System.Net.Http.HttpClient();
+            var request = new HttpRequestMessage
+            {
+                Method = HttpMethod.Post,
+                RequestUri = new Uri("https://text-translator2.p.rapidapi.com/translate"),
+                Headers =
+                {
+                    { "X-RapidAPI-Key", "9495ed9410msh7befb35f01562fcp16940cjsnd0f895b9ede1" },
+                    { "X-RapidAPI-Host", "text-translator2.p.rapidapi.com" },
+                },
+                Content = new FormUrlEncodedContent(new Dictionary<string, string>
+                {
+                    { "source_language", "en" },
+                    { "target_language", "id" },
+                    { "text", "What is your name?" },
+                }),
+            };
+            using (var response = await client.SendAsync(request))
+            {
+                response.EnsureSuccessStatusCode();
+                var body = await response.Content.ReadAsStringAsync();
+                Console.WriteLine(body);
+            }
         }
     }
 }
