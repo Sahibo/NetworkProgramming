addb2eebd260e17af1273cd04ed382cc6d6f3af0
Sahib Aliyev <sahib.aliyev.2002@gmail.com>
2023-03-16 23:00:39 +0400

    First

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace HttpClient
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
