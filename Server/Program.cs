13e73e4b61a49606035f744b129a55a6df4e23d8
Sahib Aliyev <sahib.aliyev.2002@gmail.com>
2023-06-18 02:49:26 +0400

    First commit

---
+++ Server/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.IO;
+using System.Net;
+
+class Program
+{
+    static void Main()
+    {
+        
+        string ftpHost = "ftp.sahibo.com";
+        string ftpUsername = "anonymous";
+        string ftpPassword = ""; 
+
+        string localFilePath = @"C:\Users\sahib\source\repos\NetworkProgramming\FtpServer\test.txt";
+
+        string remoteFilePath = "/test.txt";
+
+        DownloadFileFromFtpServer(ftpHost, ftpUsername, ftpPassword, remoteFilePath, localFilePath);
+
+        UploadFileToFtpServer(ftpHost, ftpUsername, ftpPassword, localFilePath, remoteFilePath);
+
+        Console.WriteLine("Done!");
+    }
+
+    static void DownloadFileFromFtpServer(string host, string username, string password, string remoteFilePath, string localFilePath)
+    {
+        using (WebClient client = new WebClient())
+        {
+            client.Credentials = new NetworkCredential(username, password);
+            client.DownloadFile($"ftp://{host}/{remoteFilePath}", localFilePath);
+        }
+    }
+
+    static void UploadFileToFtpServer(string host, string username, string password, string localFilePath, string remoteFilePath)
+    {
+        using (WebClient client = new WebClient())
+        {
+            client.Credentials = new NetworkCredential(username, password);
+            client.UploadFile($"ftp://{host}/{remoteFilePath}", localFilePath);
+        }
+    }
+}
