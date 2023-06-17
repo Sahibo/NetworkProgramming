e0d5a7916e2d3aff4f5ed4d563254f51de9bda21
Sahib Aliyev <sahib.aliyev.2002@gmail.com>
2023-06-17 21:29:27 +0400

    Done

---
+++ client/Client/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Net.Sockets;
+using System.Text;
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Enter server IP address:");
+        string ipAddress = Console.ReadLine();
+
+        Console.WriteLine("Enter server port number:");
+        int port = int.Parse(Console.ReadLine());
+
+        TcpClient client = new TcpClient(ipAddress, port);
+        NetworkStream stream = client.GetStream();
+
+        Console.WriteLine("Connected to the server.");
+
+        while (true)
+        {
+            Console.WriteLine("Enter your choice (0 - Rock, 1 - Paper, 2 - Scissors):");
+            byte[] buffer = new byte[1];
+            buffer[0] = byte.Parse(Console.ReadLine());
+
+            stream.Write(buffer, 0, buffer.Length);
+
+            byte[] opponentChoiceBuffer = new byte[1];
+            stream.Read(opponentChoiceBuffer, 0, opponentChoiceBuffer.Length);
+
+            Console.WriteLine("Opponent's choice: " + (Choice)opponentChoiceBuffer[0]);
+        }
+    }
+}
+
+enum Choice
+{
+    Rock = 0,
+    Paper = 1,
+    Scissors = 2
+}
---
+++ server/Server/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+class Program
+{
+    static void Main()
+    {
+        int port = 8888;
+        IPAddress ipAddress = IPAddress.Parse("127.0.0.1");
+        TcpListener listener = new TcpListener(ipAddress, port);
+        listener.Start();
+
+        Console.WriteLine("Server started.\n Waiting for connections...");
+
+        TcpClient client1 = listener.AcceptTcpClient();
+        Console.WriteLine("Player 1 connected.");
+
+        TcpClient client2 = listener.AcceptTcpClient();
+        Console.WriteLine("Player 2 connected.");
+
+        NetworkStream stream1 = client1.GetStream();
+        NetworkStream stream2 = client2.GetStream();
+
+        while (true)
+        {
+            byte[] buffer = new byte[1];
+            int bytesRead = stream1.Read(buffer, 0, buffer.Length);
+
+            if (bytesRead > 0)
+            {
+                Console.WriteLine("Player 1 choice: " + (Choice)buffer[0]);
+
+                stream2.Write(buffer, 0, buffer.Length);
+            }
+
+            bytesRead = stream2.Read(buffer, 0, buffer.Length);
+
+            if (bytesRead > 0)
+            {
+                Console.WriteLine("Player 2 choice: " + (Choice)buffer[0]);
+
+                stream1.Write(buffer, 0, buffer.Length);
+            }
+        }
+    }
+}
+
+enum Choice
+{
+    Rock = 0,
+    Paper = 1,
+    Scissors = 2
+}
